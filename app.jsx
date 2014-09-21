bf6d8ecd6af8fde3fb00b3eaba116e04d8ec280f
Nicolas Perriault <nperriault@gmail.com>
2014-09-21 15:59:14 +0200

    WiP

--- app.jsx
+++ app.jsx
@@ -1,55 +1,63 @@
 /** @jsx React.DOM */
 var apiKey = "dc6zaTOxFJmzC"; // data.embed_url
 
+function requestGifFromWord(word, resolve, reject) {
+  var req = new XMLHttpRequest();
+  var url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + word;
+  req.open("GET", url, true);
+  req.setRequestHeader('Content-Type', 'application/json');
+  req.setRequestHeader('Accept', 'application/json');
+  req.responseType = 'json';
+  req.onload = function() {
+    try {
+      resolve({
+        word: word,
+        image: req.response.data.image_original_url
+      });
+    } catch (error) {
+      resolve({word: word});
+    }
+  };
+  req.onerror = req.ontimeout = function(event) {
+    reject({
+      status: event.target.status,
+      message: event.target.statusText,
+      response: event.target,
+    });
+  };
+  req.send();
+}
+
 var Form = React.createClass({
+  getDefaultProps: function() {
+    return {text: ""};
+  },
+
+  getInitialState: function() {
+    return {text: this.props.text || ""};
+  },
+
   handleSubmit: function(event) {
     event.preventDefault();
     var text = this.refs.text.getDOMNode().value;
-    var slides = text.split("\n").filter(function(word) {
+    var words = text.split("\n").filter(function(word) {
       return !!word.trim();
     });
-    var promises = slides.map(function(word) {
-      return new Promise(function(resolve, reject) {
-        var req = new XMLHttpRequest();
-        var url = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + word;
-        req.open("GET", url, true);
-        req.setRequestHeader('Content-Type', 'application/json');
-        req.setRequestHeader('Accept', 'application/json');
-        req.responseType = 'json';
-        req.onload = function() {
-          try {
-            resolve({
-              word: word,
-              image: req.response.data.image_original_url
-            });
-          } catch (error) {
-            resolve({word: word});
-          }
-        };
-        req.onerror = req.ontimeout = function(event) {
-          reject({
-            status: event.target.status,
-            message: event.target.statusText,
-            response: event.target,
-          });
-        };
-        req.send();
-      });
-    });
-    Promise.all(promises).then(function(slidesInfo) {
-      this.props.onSlideInfoReceived(slidesInfo);
-    }.bind(this));
+    this.props.onWordsReceived(words);
+    document.location.hash = words.join(",");
   },
 
-  componentDidMount: function() {
-
+  handleTextChange: function(event) {
+    console.log("text changed", event.target.value);
+    this.setState({text: event.target.value});
   },
 
   render: function() {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
-          <textarea ref="text" defaultValue="cat" />
+          <textarea ref="text" value={this.state.text}
+                    onChange={this.handleTextChange} />
         </label>
         <button>Submit</button>
       </form>
@@ -81,24 +89,65 @@ var Slide = React.createClass({
 
 var Slideshow = React.createClass({
   getInitialState: function() {
-    return {slides: []};
+    return {
+      words: this.getWordsFromUrlHash(),
+      slides: []
+    };
   },
+
+  getWordsFromUrlHash: function() {
+    var hash = document.location.hash;
+    if (!hash || hash.length === 0) {
+      return [];
+    }
+    return hash.slice(1).split(",");
+  },
+
+  // shouldComponentUpdate: function(nextProps, nextState) {
+  //   return nextState.words !== this.state.words;
+  // },
 
   componentDidUpdate: function() {
-    Reveal.initialize();
+    try {
+      Reveal.initialize();
+    } catch (e) {
+      console.error(e.message);
+    }
   },
 
-  onSlideInfoReceived: function(slidesInfo) {
-    this.setState({slides: slidesInfo});
+  componentDidMount: function() {
+    var words = this.getWordsFromUrlHash();
+    this.onWordsReceived(words);
+    window.addEventListener("hashchange", function(event) {
+      console.log("hash words changed", this.getWordsFromUrlHash());
+      var words = this.getWordsFromUrlHash();
+      this.onWordsReceived(words);
+    }.bind(this));
+  },
+
+  onWordsReceived: function(words) {
+    this.setState({slides: [], words: words});
+    var promises = words.map(function(word) {
+      return new Promise(function(resolve, reject) {
+        requestGifFromWord(word, resolve, reject);
+      });
+    });
+    Promise.all(promises).then(function(slidesInfo) {
+      this.setState({slides: slidesInfo});
+    }.bind(this));
   },
 
   render: function() {
+    console.log('Slideshow#state:', this.state);
+    console.log("joined", this.state.words.join("\n"));
     var slidesComponents = this.state.slides.map(function(slide, i) {
       return <Slide key={i} word={slide.word} image={slide.image} />;
     }.bind(this));
+    // XXX: bug: Form component should be notified of words state changes
     return (
       <div>
-        <Form onSlideInfoReceived={this.onSlideInfoReceived} />
+        <Form text={this.state.words.join("\n")}
+              onWordsReceived={this.onWordsReceived} />
         <div className="reveal">
           <div className="slides">
             {slidesComponents}
